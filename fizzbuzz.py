d79ce57d896678e7bb9be126bec626f67731878f
Sanderella <62627476+Sanderella@users.noreply.github.com>
2020-06-06 10:34:49 +0200

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+count=1
+
+while(count<=100):
+    if count % 3 == 0 and count % 5 == 0:
+        print("FizzBuzz")
+    elif count % 3 == 0:
+        print("Fizz")
+    elif count % 5 == 0:
+        print("Buzz")
+
+    else:
+        print(count)
+
+    count=count+1
+
